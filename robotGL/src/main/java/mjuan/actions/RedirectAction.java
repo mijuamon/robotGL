a276bb88eab77d19ff51e321325e4625b471ad30
mjuan <mjuan@ITEM-83600.dhcp.vale.es.sopra>
2015-12-03 13:25:48 +0100

    Implementado sistema de redireccion

--- robotGL/src/main/java/mjuan/actions/RedirectAction.java
+++ robotGL/src/main/java/mjuan/actions/RedirectAction.java
@@ -1,7 +1,10 @@
 package mjuan.actions;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.Action;
 
@@ -10,8 +13,11 @@ public class RedirectAction implements Action
 	private static final Logger log = LogManager.getLogger(BrazoAction.class);
 
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		HttpSession session = ServletActionContext.getRequest().getSession();
+		String forward = (String) session.getAttribute("forward");
+		log.info("going to: " + forward);
+		return forward;
 	}
 
 }
--- robotGL/src/main/webapp/selectBrazo.jsp
+++ robotGL/src/main/webapp/selectBrazo.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-BINGO
+<h1>BINGO</h1>
 </body>
 </html>
