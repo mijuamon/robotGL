5d8e4f6131f03969179d7cec6018cc838b5d1b8c
mjuan <mjuan@ITEM-83600.dhcp.vale.es.sopra>
2015-12-03 15:35:05 +0100

    Agregadas imagenes para la galeria

---
+++ robotGL/src/main/webapp/js/webGL.js
@@ -0,0 +1,110 @@
+var width = window.innerWitdh,
+	height = window.innerHeight,
+	clock = new THREE.Clock(),
+	scene,
+	camera,
+	renderer,
+	ambientLight,
+	directionalLight,
+	loader,
+	modelo = new THREE.Object3D(),
+	cameraConmtrols,
+	URL;
+
+	init();
+	animate();	
+
+
+function init()
+{
+	
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(width,height);	
+	renderer.setClearColor(new THREE.Color(0x0000AA),1.0);
+	document.getElementById('container').appendChild(renderer.domElement);
+	
+	scene =new THREE.Scene();
+	var aspectRatio = window.innerWidth/window.innerHeight;
+	
+	camera = new THREE.PerspectiveCamera(45, aspectRatio,0.1,100);
+	camera.position.set(0,5,30);
+	camera.lookAt(new THREE.Vector3(0,0,0));
+	
+	ambientLight = new THREE.AmbientLight(0xffffff);
+	scene.add(ambientLight);
+	
+	directionalLight = new THREE.DirectionalLight(0xffffff);
+	directionalLight.position.set(0,1,0);
+	scene.add(directionalLight);
+	
+	scene.add(new THREE.GridHelper(10,1));
+	loader = new THREE.JSONLoader();	
+	
+	
+	//Cubo de prueba
+	/*var material = new THREE.MeshBasicMaterial({color:0xFF0000, wireframe:true});
+	var geometriaCubo = new THREE.CubeGeometry(2,2,2);
+	var cubo = new THREE.Mesh(geometriaCubo,material);
+	cubo.position.x=-1;
+	scene.add(cubo);*/
+	
+	
+	
+	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+	cameraControls.target.set(0,0,0);
+	window.addEventListener('resize',updateAspectRatio);
+	
+	
+}
+
+function updateAspectRatio()
+{
+	//Renovar las dimensiones del renderer
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	//Renovar la relacion de aspecto de frutum
+	camera.aspect = window.innerWidth / window.innerHeight;
+	
+	camera.updateProjectionMatrix();
+}
+function animate() {
+
+    requestAnimationFrame( animate );
+
+   // modelo.rotation.x += 0.05;
+
+    renderer.render( scene, camera );
+}
+
+function setURL(url)
+{
+	//window.stop()
+	URL = url+'/model.json';	
+	
+	loader.load(URL, function(geometry,materials)
+	{
+		//Cargamos el modelo
+		modelo = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+		
+		
+		//Cargamos el archivo de configuracion del modelo
+		//------------------------------------
+	
+		$.get(url+'/config.cfg', function(result) 
+		{
+			var txt=result.split('\n');
+			var pos=txt[2].split(':')[1].split(',');
+			var rot=txt[3].split(':')[1].split(',');			
+			var sca=txt[4].split(':')[1].split(',');
+				
+			modelo.position.set(parseInt(pos[0]),parseInt(pos[1]),parseInt(pos[2]));
+			modelo.scale.set(parseInt(sca[0]),parseInt(sca[1]),parseInt(sca[2]));
+		});
+
+		//------------------------------------
+	
+		scene.add(modelo);		
+	});	
+}
+
+
---
+++ robotGL/src/main/webapp/showBrazo.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		<script src="libs/threejs/three.min.js"></script>
+		<script src="libs/threejs/OrbitControls.js"></script>
+		<script src="libs/threejs/Coordinates.js"></script>
+		<script src="libs/threejs/dat.gui.min.js"></script>
+		<script src="libs/threejs/stats.min.js"></script>
+		<script src="js/json.js"></script>
+		BINGO BRAZO
+		<div id='container' width=800, height=600>	
+	</body>
+</html>
